5c8f881e9db0ca5a24bdeb1d0b9f8c8d0eda847e
mdy666 <maduyue1031@gmail.com>
2025-03-12 21:14:40 +0800

    Update fused_apply_rope.py
    fix rope error

--- triton_kernel/fused_apply_rope.py
+++ triton_kernel/fused_apply_rope.py
@@ -4,22 +4,10 @@ import triton.language as tl
 
 
 @triton.jit
-def _fused_apply_rope_fwd(Q, 
-                          K, 
-                          COS, 
-                          SIN,
-                          Q_EMBED, 
-                          K_EMBED,
-                          B, 
-                          COS_B, 
-                          L, 
-                          QH, 
-                          KH, 
-                          D:tl.constexpr, 
-                          HALF_D:tl.constexpr,
-                          BLOCK_QH:tl.constexpr, 
-                          BLOCK_KH:tl.constexpr, 
-                          BLOCK_D:tl.constexpr,
+def _fused_apply_rope_fwd(Q, K, COS, SIN,
+                          Q_EMBED, K_EMBED,
+                          B, COS_B, L, QH, KH, D:tl.constexpr, HALF_D:tl.constexpr,
+                          BLOCK_QH:tl.constexpr, BLOCK_KH:tl.constexpr, BLOCK_D:tl.constexpr,
                           CHUNK_N:tl.constexpr=64
                           ):
     start_n = tl.cast(tl.program_id(0), tl.int64) * CHUNK_N + tl.program_id(1)
@@ -61,23 +49,13 @@ def _fused_apply_rope_fwd(Q,
     
 
 @triton.jit
-def _fused_apply_rope_bwd(  DQ_EMBED, 
-                            DK_EMBED, 
-                            COS, 
-                            SIN,
-                            DQ, 
-                            DK, 
-                            B, 
-                            COS_B, 
-                            L, 
-                            QH, 
-                            KH, 
-                            D: tl.constexpr, 
-                            HALF_D: tl.constexpr,
-                            BLOCK_QH:tl.constexpr, 
-                            BLOCK_KH:tl.constexpr, 
-                            BLOCK_D:tl.constexpr,
-                            CHUNK_N:tl.constexpr=64
+def _fused_apply_rope_bwd(DQ_EMBED, DK_EMBED, COS, SIN,
+                          DQ, DK, 
+                          dq_stride_b, dq_stride_h, dq_stride_l, dq_stride_d,
+                          dk_stride_b, dk_stride_h, dk_stride_l, dk_stride_d,
+                        B, COS_B, L, QH, KH, D: tl.constexpr, HALF_D: tl.constexpr,
+                        BLOCK_QH:tl.constexpr, BLOCK_KH:tl.constexpr, BLOCK_D:tl.constexpr,
+                        CHUNK_N:tl.constexpr=64
                         ):
     start_n = tl.cast(tl.program_id(0), tl.int64) * CHUNK_N + tl.program_id(1)
     if start_n >= B*L:
@@ -85,10 +63,10 @@ def _fused_apply_rope_bwd(  DQ_EMBED,
     off_b = start_n // L
     off_l = start_n % L
 
-    DQ += start_n * QH * D
-    DQ_EMBED += start_n * QH * D
-    DK += start_n * KH * D
-    DK_EMBED += start_n * KH * D
+    DQ += off_b * dq_stride_b + off_l * dq_stride_l
+    DQ_EMBED += off_b * dq_stride_b + off_l * dq_stride_l
+    DK += off_b * dk_stride_b + off_l * dk_stride_l
+    DK_EMBED += off_b * dk_stride_b + off_l * dk_stride_l
     COS += off_b * (COS_B // B) * L * D + off_l * D
     SIN += off_b * (COS_B // B) * L * D + off_l * D
 
@@ -98,23 +76,23 @@ def _fused_apply_rope_bwd(  DQ_EMBED,
     sin1 = tl.load(SIN + cols, mask=cols<HALF_D, other=0.).to(tl.float32)[None, :]
     sin2 = tl.load(SIN + cols + HALF_D, mask=(cols + HALF_D)<D, other=0.).to(tl.float32)[None, :]
 
-    dk_embed1 = tl.load(DK_EMBED + tl.arange(0, BLOCK_KH)[:, None] * D + cols[None, :], 
+    dk_embed1 = tl.load(DK_EMBED + tl.arange(0, BLOCK_KH)[:, None] * dk_stride_h + cols[None, :], 
                  mask=(cols[None, :]<HALF_D) & (tl.arange(0, BLOCK_KH)[:, None] < KH), other=0.).to(tl.float32)
-    dk_embed2 = tl.load(DK_EMBED + tl.arange(0, BLOCK_KH)[:, None] * D + cols[None, :] + HALF_D, 
+    dk_embed2 = tl.load(DK_EMBED + tl.arange(0, BLOCK_KH)[:, None] * dk_stride_h + cols[None, :] + HALF_D, 
                  mask=((HALF_D+cols[None, :])<D) & (tl.arange(0, BLOCK_KH)[:, None] < KH), other=0.).to(tl.float32)
     dk1 = dk_embed1 * cos1 + sin2 * dk_embed2
     dk2 = dk_embed2 * cos2 - sin1 * dk_embed1
-    tl.store(DK + tl.arange(0, BLOCK_KH)[:, None] * D + cols[None, :], dk1, mask=(cols[None, :]<HALF_D)&(tl.arange(0, BLOCK_KH)[:, None] < KH))
-    tl.store(DK + tl.arange(0, BLOCK_KH)[:, None] * D + cols[None, :] + HALF_D, dk2, mask=((HALF_D+cols[None, :])<D) & (tl.arange(0, BLOCK_KH)[:, None] < KH))
+    tl.store(DK + tl.arange(0, BLOCK_KH)[:, None] * dk_stride_h + cols[None, :], dk1, mask=(cols[None, :]<HALF_D)&(tl.arange(0, BLOCK_KH)[:, None] < KH))
+    tl.store(DK + tl.arange(0, BLOCK_KH)[:, None] * dk_stride_h + cols[None, :] + HALF_D, dk2, mask=((HALF_D+cols[None, :])<D) & (tl.arange(0, BLOCK_KH)[:, None] < KH))
 
-    dq_embed1 = tl.load(DQ_EMBED + tl.arange(0, BLOCK_QH)[:, None] * D + cols[None, :], 
+    dq_embed1 = tl.load(DQ_EMBED + tl.arange(0, BLOCK_QH)[:, None] * dq_stride_h + cols[None, :], 
                  mask=(cols[None, :]<HALF_D) & (tl.arange(0, BLOCK_QH)[:, None] < QH), other=0.).to(tl.float32)
-    dq_embed2 = tl.load(DQ_EMBED + tl.arange(0, BLOCK_QH)[:, None] * D + cols[None, :] + HALF_D, 
+    dq_embed2 = tl.load(DQ_EMBED + tl.arange(0, BLOCK_QH)[:, None] * dq_stride_h + cols[None, :] + HALF_D, 
                  mask=((HALF_D+cols[None, :])<D) & (tl.arange(0, BLOCK_QH)[:, None] < QH), other=0.).to(tl.float32)
     dq1 = dq_embed1 * cos1 + sin2 * dq_embed2
     dq2 = dq_embed2 * cos2 - sin1 * dq_embed1
-    tl.store(DQ + tl.arange(0, BLOCK_QH)[:, None] * D + cols[None, :], dq1, mask=(cols[None, :]<HALF_D)&(tl.arange(0, BLOCK_QH)[:, None] < QH))
-    tl.store(DQ + tl.arange(0, BLOCK_QH)[:, None] * D + cols[None, :] + HALF_D, dq2, mask=((HALF_D+cols[None, :])<D) & (tl.arange(0, BLOCK_QH)[:, None] < QH))
+    tl.store(DQ + tl.arange(0, BLOCK_QH)[:, None] * dq_stride_h + cols[None, :], dq1, mask=(cols[None, :]<HALF_D)&(tl.arange(0, BLOCK_QH)[:, None] < QH))
+    tl.store(DQ + tl.arange(0, BLOCK_QH)[:, None] * dq_stride_h + cols[None, :] + HALF_D, dq2, mask=((HALF_D+cols[None, :])<D) & (tl.arange(0, BLOCK_QH)[:, None] < QH))
 
 class _FusedApplyRope(torch.autograd.Function):
     @staticmethod
@@ -129,9 +107,9 @@ class _FusedApplyRope(torch.autograd.Function):
         num_stages=4
         num_warps=8
 
-        # print(qr.stride())
         q_embed = torch.empty(B, L, QH, D, device=q.device, dtype=k.dtype)
         k_embed = torch.empty(B, L, KH, D, device=q.device, dtype=k.dtype)
+        
         N = B * L 
         COS_B = cos.shape[0]
         BLOCK_QH = triton.next_power_of_2(QH)
@@ -153,7 +131,7 @@ class _FusedApplyRope(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, dq_embed, dk_embed):
-        # print(dq_embed.stride(), dk_embed.stride())
+        print(dq_embed.stride(), dk_embed.shape)
         B, QH, KH, L, D, HALF_D, N, COS_B, BLOCK_QH, BLOCK_KH, BLOCK_D = ctx.infos
         cos,sin = ctx.saved_tensors
         dq = torch.empty_like(dq_embed)
@@ -161,6 +139,8 @@ class _FusedApplyRope(torch.autograd.Function):
         grid = lambda meta: (triton.cdiv(N, meta['CHUNK_N']), meta['CHUNK_N'])
         _fused_apply_rope_bwd[grid](dq_embed, dk_embed, cos, sin,
                                     dq, dk,
+                                    *dq.stride(),
+                                    *dk.stride(),
                                     B, COS_B, L, QH, KH, D, HALF_D, BLOCK_QH, BLOCK_KH, BLOCK_D,
                                     num_warps=ctx.num_warps, num_stages=ctx.num_stages
                                     )
